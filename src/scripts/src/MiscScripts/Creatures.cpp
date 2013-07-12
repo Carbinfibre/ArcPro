3594e2cdda3c30535c29f347684ddc25444488c6
zdroid9770 <zdroid9770@yahoo.com>
2013-07-12 14:58:57 -0500

    MiscScripts

--- src/scripts/src/MiscScripts/Creatures.cpp
+++ src/scripts/src/MiscScripts/Creatures.cpp
@@ -1,21 +1,23 @@
 /*
-* Moon++ Scripts for Ascent MMORPG Server
-* Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
-* Copyright (C) 2007-2008 Moon++ Team <http://www.moonplusplus.info/>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * ArcPro MMORPG Server
+ * Copyright (C) 2011-2013 ArcPro Speculation <http://arcpro.sexyi.am/>
+ * Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
+ * Copyright (C) 2007-2008 Moon++ Team <http://www.moonplusplus.info/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include "Setup.h"
 
@@ -71,7 +73,7 @@ class SavannahProwler : public CreatureAIScript
 				_unit->SetStandState(0);
 		}
 
-		static CreatureAIScript* Create(Creature* c) { return new SavannahProwler(c); }
+		static CreatureAIScript* Create(Creature* c) { return new SavannahProwler(C); }
 };
 
 //Lazy Peons
@@ -305,7 +307,7 @@ class DISCO : public CreatureAIScript
 {
 	public:
 		ADD_CREATURE_FACTORY_FUNCTION(DISCO);
-		DISCO(Creature* c) : CreatureAIScript(c) {}
+		DISCO(Creature* c) : CreatureAIScript(C) {}
 
 		void OnLoad()
 		{
--- src/scripts/src/MiscScripts/GameObjectTeleportTable.cpp
+++ src/scripts/src/MiscScripts/GameObjectTeleportTable.cpp
@@ -1,20 +1,22 @@
 /*
- * ArcScript Scripts for Arcemu MMORPG Server
- * Copyright (C) 2008-2011 Arcemu Team
- * Copyright (C) 2007 Moon++ <http://www.moonplusplus.com/>
+ * ArcPro MMORPG Server
+ * Copyright (C) 2011-2013 ArcPro Speculation <http://arcpro.sexyi.am/>
+ * Copyright (C) 2008-2013 ArcEmu Team <http://www.arcemu.org/>
+ * Copyright (C) 2007-2008 Moon++ Team <http://www.moonplusplus.info/>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 /*
